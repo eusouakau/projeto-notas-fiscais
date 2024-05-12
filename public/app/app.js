52adb0fe6e7efcb41b273765b62780287d63c141
eusouakau <karoltvbl@gmail.com>
2024-05-12 20:49:27 -0300

    trata caso de reject

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,8 @@
 document.querySelector('#myButton').onclick = () => 
     fetch('http://localhost:3000/notas')
-    .then(res => res.json())
+    .then(res => {
+        if(res.ok) return res.json();
+        return Promise.reject(res.statusText);
+    })
     .then(notas => console.log(notas))
     .catch(err => console.log(err));
