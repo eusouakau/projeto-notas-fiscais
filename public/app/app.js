9fe597e97ab9872f4a6e71f6d4b870571aced6c3
eusouakau <karoltvbl@gmail.com>
2024-05-12 21:07:01 -0300

    simplifica

--- public/app/app.js
+++ public/app/app.js
@@ -6,5 +6,5 @@ document.querySelector('#myButton').onclick = () =>
     .then(notas => notas.map(nota => nota.itens))
     .then(itens => itens.filter(item => item.codigo == '2143'))
     .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-    .then(total => console.log(total))
-    .catch(err => console.log(err));
+    .then(console.log)
+    .catch(console.log);
