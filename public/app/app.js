de8909e49588cbc8708d5114969bc8343953c2f2
eusouakau <karoltvbl@gmail.com>
2024-05-12 21:06:36 -0300

    add map, filter e reduce

--- public/app/app.js
+++ public/app/app.js
@@ -3,5 +3,8 @@ import { handleStatus } from "./utils/promiseHelpers.js";
 document.querySelector('#myButton').onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => console.log(notas))
+    .then(notas => notas.map(nota => nota.itens))
+    .then(itens => itens.filter(item => item.codigo == '2143'))
+    .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+    .then(total => console.log(total))
     .catch(err => console.log(err));
