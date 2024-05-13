9281593c86692be7f12f8dfc4263b8ec18f86849
eusouakau <karoltvbl@gmail.com>
2024-05-12 21:10:10 -0300

    substitui map por reduce para que o array se mantenha com uma dimensao

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ import { handleStatus } from "./utils/promiseHelpers.js";
 document.querySelector('#myButton').onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas.map(nota => nota.itens))
+    .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
     .then(itens => itens.filter(item => item.codigo == '2143'))
     .then(itens => itens.reduce((total, item) => total + item.valor, 0))
     .then(console.log)
