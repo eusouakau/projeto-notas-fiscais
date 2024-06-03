880aa19b2ddb64af7c16040f6bb81cc3289012e9
eusouakau <karoltvbl@gmail.com>
2024-06-03 20:50:20 -0300

    remove a responsabilidade de fazer a soma dos itens

--- public/app/app.js
+++ public/app/app.js
@@ -2,13 +2,7 @@ import { handleStatus, log } from "./utils/promiseHelpers.js";
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
-
 document.querySelector('#myButton').onclick = () => 
-    service.listAll()
-    .then(sumItems('2143'))
+    service.sumItems('2143')
     .then(console.log)
     .catch(console.log);
