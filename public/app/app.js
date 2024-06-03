510500cb18c50cfa3455d024a21fc0cec9cad731
eusouakau <karoltvbl@gmail.com>
2024-06-03 20:28:17 -0300

    separa a soma dos itens em uma função

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,13 @@
 import { handleStatus, log } from "./utils/promiseHelpers.js";
 import './utils/arrayHelpers.js';
 
+const sumItems = notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == '2143')
+    .reduce((total, item) => total + item.valor, 0);
 document.querySelector('#myButton').onclick = () => 
     fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then(notas => notas
-        .$flatMap(nota => nota.itens)
-        .filter(item => item.codigo == '2143')
-        .reduce((total, item) => total + item.valor, 0))
+    .then(sumItems)
     .then(console.log)
     .catch(console.log);
