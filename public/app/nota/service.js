af36e7f4a06cbe6b0d3b9409a954a3b293bd900e
eusouakau <karoltvbl@gmail.com>
2024-06-06 20:59:48 -0300

    add mensagem de erro de alto nivel

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -8,7 +8,12 @@ const filtersItemsByCode = (code, items) => items.filter(item => item.codigo == 
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
-    listAll() { return fetch(API).then(handleStatus); },
+    listAll() { 
+        return fetch(API).then(handleStatus).catch(err => {
+            console.log(err)
+            return Promise.reject('Não foi possível obter as notas fiscais.');
+        });
+    },
     sumItems(code) { 
         const filterItems = partialize(filtersItemsByCode, code);
         return this.listAll()
