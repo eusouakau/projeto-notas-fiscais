bee68e3731fe2b932d34511facf9b64008cd4e38
eusouakau <karoltvbl@gmail.com>
2024-06-06 21:10:24 -0300

    melhora a composição

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,5 +1,5 @@
 import { handleStatus } from '../utils/promiseHelpers.js';
-import { partialize } from '../utils/operators.js';
+import { partialize, compose } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
@@ -16,7 +16,8 @@ export const notasService = {
     },
     sumItems(code) { 
         const filterItems = partialize(filtersItemsByCode, code);
+        const sumItems = compose(sumItemsValue, filterItems, getItemsFromNotas);
         return this.listAll()
-            .then(notas => sumItemsValue(filterItems(getItemsFromNotas(notas))));
+            .then(sumItems);
     }
 }
