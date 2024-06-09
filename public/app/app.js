f60b01841f68fbcd9b818b56de48bc46affd2d2d
eusouakau <karoltvbl@gmail.com>
2024-06-08 23:31:26 -0300

    cria composicao para organizar o codigo

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,10 @@
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 import { log } from './utils/promiseHelpers.js';
 
-const action = debounceTime(500,takeUntil(3, () => service.sumItems('2143').then(log).catch(log)));
+const operations = pipe(partialize(takeUntil, 3), partialize(debounceTime, 500));
+const action = operations(() => service.sumItems('2143').then(log).catch(log));
 
 
 document.querySelector('#myButton').onclick = action;
