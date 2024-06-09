35ede2d901ce0cf839b668b27d62fe4936ce5a64
eusouakau <karoltvbl@gmail.com>
2024-06-08 23:49:23 -0300

    add timeout

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,9 @@
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
-import { log } from './utils/promiseHelpers.js';
+import { log, timeoutPromise } from './utils/promiseHelpers.js';
 
 const operations = pipe(partialize(takeUntil, 3), partialize(debounceTime, 500));
-const action = operations(() => service.sumItems('2143').then(log).catch(log));
+const action = operations(() => timeoutPromise(200, service.sumItems('2143')).then(log).catch(log));
 
 document.querySelector('#myButton').onclick = action;
