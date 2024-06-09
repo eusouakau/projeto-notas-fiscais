0559a55a32692fc51a1ecdbde681c532a3ce8887
eusouakau <karoltvbl@gmail.com>
2024-06-08 22:57:29 -0300

    limita busca de notas

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,8 @@
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
+import { log } from './utils/promiseHelpers.js';
 
-document.querySelector('#myButton').onclick = () => 
-    service.sumItems('2143')
-    .then(console.log)
-    .catch(console.log);
+const operation = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
+
+document.querySelector('#myButton').onclick = operation;
