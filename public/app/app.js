1fe062be65de9102a951dad696113fda4b7bb13f
eusouakau <karoltvbl@gmail.com>
2024-06-09 00:19:30 -0300

    add retry

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,10 @@
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
-import { log, timeoutPromise } from './utils/promiseHelpers.js';
+import { log, timeoutPromise, retry } from './utils/promiseHelpers.js';
 
 const operations = pipe(partialize(takeUntil, 3), partialize(debounceTime, 500));
-const action = operations(() => timeoutPromise(200, service.sumItems('2143')).then(log).catch(log));
+const action = operations(() => retry(3, 300, () => timeoutPromise(200, service.sumItems('2143'))
+    .then(log).catch(log)));
 
 document.querySelector('#myButton').onclick = action;
