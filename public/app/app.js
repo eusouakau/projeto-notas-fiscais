df0ab1e2acfecf1ce9b02da26f91f43e04524acf
eusouakau <karoltvbl@gmail.com>
2024-06-08 23:18:08 -0300

    limita operacoes com cliques repetitivos

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,9 @@
 import './utils/arrayHelpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 import { log } from './utils/promiseHelpers.js';
 
-const operation = takeUntil(3, () => service.sumItems('2143').then(log).catch(log));
+const action = debounceTime(500,takeUntil(3, () => service.sumItems('2143').then(log).catch(log)));
 
-document.querySelector('#myButton').onclick = operation;
+
+document.querySelector('#myButton').onclick = action;
